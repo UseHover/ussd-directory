467c621fd8bf9e58eb0dd8c5283eea72144962a2
Samuel Ralak <thesamuelralak@gmail.com>
2022-07-28 13:39:42 +0300

    182751888 moved banner content into component

--- packs/pages/Home.js
+++ packs/pages/Home.js
@@ -1,90 +1,19 @@
-import React, { useContext, useEffect, useRef, useState } from 'react'
-import Header from '../components/shared/Header'
-import { CountriesContext } from '../components'
+import React from 'react'
+import { BannerContent, CountrySearchInput, Header } from '../components/shared'
 
 const Home = () => {
-  const countriesContext = useContext(CountriesContext)
-  const [countries, setCountries] = useState([])
-  const countryListRef = useRef(null)
-
-  const countrySearchOnBlur = () => {
-    countryListRef.current.classList.remove('uk-open')
-  }
-
-  const countrySearchOnClick = () => {
-    const classList = countryListRef.current.classList
-
-    if (classList.contains('uk-open')) {
-      classList.remove('uk-open')
-      return
-    }
-
-    classList.add('uk-open')
-  }
-
-  const filterCountries = event => {
-    countryListRef.current.classList.add('uk-open')
-    const value = event.currentTarget.value.toLowerCase()
-
-    if (value.length === 0) {
-      setCountries(countriesContext.countries)
-      return
-    }
-
-    setCountries([
-      ...countries.filter(c => {
-        const name = c.name.toLowerCase()
-        return name.indexOf(value) !== -1 || name === value
-      }),
-    ])
-  }
-
-  useEffect(() => setCountries(countriesContext.countries), [countriesContext.countries])
-
   return (
     <>
       <section className="home-banner">
         <Header />
         <article className="tx-c co-white">
           <div className="wrapper will-grow">
-            <h1 className="co-white mb-1h">USSD Directory for 30+ countries</h1>
-            <p className="max-527 mx-auto lh-31">
-              Find USSD codes across Africa. We have thousands of entries across mobile network operators, banks,
-              telcos, industries and utilities.
-            </p>
-            <section className="max-667 mx-auto pos-r">
-              <form className="uk-search uk-search-default mt-5" type="button" id="searchCountryForm">
-                <a href="" className="uk-search-icon">
-                  <svg width="24" height="24">
-                    <use href="/uploads/icon-sprite.svg#search"></use>
-                  </svg>
-                </a>
-
-                <input
-                  id="searchInput"
-                  className="uk-search-input dropbtn bg-white"
-                  placeholder="Search country"
-                  autoComplete="off"
-                  onClick={countrySearchOnClick}
-                  onBlur={countrySearchOnBlur}
-                  onInput={filterCountries}
-                />
-              </form>
-              <ul
-                ref={countryListRef}
-                className="bg-white search-drop dropdown-content uk-drop uk-drop-bottom-left"
-                style={{ left: '49px', top: '70px' }}
-                id="countrySearch"
-              >
-                {countries.map(country => (
-                  <li className="d-flx al-i-c p-2 py-1 country" key={country.alpha2}>
-                    <span className="country">{String.fromCodePoint(...country.codePoints)}</span>
-                    <p className="co-black ff-medium nanotext">{country.name}</p>
-                  </li>
-                ))}
-              </ul>
-            </section>
-            .
+            <BannerContent
+              title="USSD Directory for 30+ countries"
+              subtitle="Find USSD codes across Africa. We have thousands of entries across mobile network operators, banks,
+              telcos, industries and utilities."
+            />
+            <CountrySearchInput />
           </div>
         </article>
       </section>
