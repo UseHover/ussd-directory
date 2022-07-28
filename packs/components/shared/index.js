9f16d8b1bd6831dea95d8831860319c26a505915
Samuel Ralak <thesamuelralak@gmail.com>
2022-07-28 13:43:43 +0300

    182751888 moved countrySearch into component

---
+++ packs/components/shared/index.js
@@ -0,0 +1,6 @@
+import CountrySearchInput from './CountrySearchInput'
+import Header from './Header'
+import BannerContent from './BannerContent'
+import Loader from './Loader'
+
+export { BannerContent, CountrySearchInput, Header, Loader }
