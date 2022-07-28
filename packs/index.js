a62c687af5e79ccfb3d0f679bdb5e9e1adf862e6
Samuel Ralak <thesamuelralak@gmail.com>
2022-07-28 13:42:02 +0300

    182751888 created api custom hook

--- packs/index.js
+++ packs/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import App from './App'
 import { CountriesProvider } from './components'
 
+const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('appRoot'))
 
 root.render(
   <React.StrictMode>
-    <CountriesProvider>
-      <App />
-    </CountriesProvider>
+    <QueryClientProvider client={queryClient}>
+      <CountriesProvider>
+        <App />
+      </CountriesProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 )
 
