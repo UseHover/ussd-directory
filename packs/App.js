e4d3f24b24d531c21f76d4e4332c39a8ffa7e112
Samuel Ralak <thesamuelralak@gmail.com>
2022-07-28 13:38:56 +0300

    182751888 added directory country route

--- packs/App.js
+++ packs/App.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Directory, DirectoryDetail, Home } from './pages'
+import { Directory, DirectoryCountry, DirectoryDetail, Home } from './pages'
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/directory" element={<Directory />} />
-      <Route path="/directory-country" element={<DirectoryDetail />} />
+      <Route path="/directory-country" element={<DirectoryCountry />} />
+      <Route path="/directory-detail" element={<DirectoryDetail />} />
     </Routes>
   </BrowserRouter>
 )
