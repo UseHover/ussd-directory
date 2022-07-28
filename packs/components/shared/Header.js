cc71079e9ddd158b3a8c3b0247edbc8590a27c89
Samuel Ralak <thesamuelralak@gmail.com>
2022-07-28 19:50:32 +0300

    182751888 fix mobile navigation

--- packs/components/shared/Header.js
+++ packs/components/shared/Header.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import nav from '../../../_data/nav.yml'
 import PropTypes from 'prop-types'
 
 const Header = ({ hasBanner }) => {
+  const mobileMenuRef = useRef(null)
+
+  const toggleMobileMenu = () => mobileMenuRef.current.classList.toggle('uk-open')
+  const closeMobileMenu = () => mobileMenuRef.current.classList.remove('uk-open')
+
   return (
     <>
       <header className="header space-between-groups mx-auto">
@@ -25,16 +30,21 @@ const Header = ({ hasBanner }) => {
             </a>
           </li>
         </ul>
-        <a href="#offcanvas-usage" className="hide-mediumup">
+        <a onClick={toggleMobileMenu} className="hide-mediumup">
           <svg className="co-blue" width="25" height="15">
             <use href="/uploads/icon-sprite.svg#hamburger"></use>
           </svg>
         </a>
       </header>
 
-      <div id="offcanvas-usage">
-        <div className="uk-offcanvas-bar">
-          <button className="uk-offcanvas-close" type="button"></button>
+      <div id="offcanvas-usage" ref={mobileMenuRef} className="uk-offcanvas" style={{ display: 'block' }} tabIndex="-1">
+        <div className="uk-offcanvas-bar uk-offcanvas-bar-animation uk-offcanvas-slide">
+          <button className="uk-offcanvas-close uk-icon uk-close" type="button" onClick={closeMobileMenu}>
+            <svg width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+              <line fill="none" stroke="#000" strokeWidth="1.1" x1="1" y1="1" x2="13" y2="13"></line>
+              <line fill="none" stroke="#000" strokeWidth="1.1" x1="13" y1="1" x2="1" y2="13"></line>
+            </svg>
+          </button>
           <ul className="nav hide-mediumup">
             {nav['main-nav'].map((navItem, index) => (
               <li key={index}>
