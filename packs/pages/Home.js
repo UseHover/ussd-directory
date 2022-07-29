f4e60d38e115601c990bfc0a6f7ba1f2e1241450
Samuel Ralak <thesamuelralak@gmail.com>
2022-07-29 13:32:34 +0300

    182751888 fixed blog posts on home page

--- packs/pages/Home.js
+++ packs/pages/Home.js
@@ -33,44 +33,6 @@ const Home = () => {
       <section>
         <ChannelList channels={channels} />
       </section>
-
-      <section className="wrapper will-grow d-flx gsm-section bg-lightblue">
-        <div className="mb-2 width-100-pc max-474">
-          <h2 className="mb-1h mt-1">Get Stax</h2>
-          <p className="lh-31 mb-2h">
-            Never dial a USSD Code Again. Let Stax do it for you. Stax makes it simpler and easier for you to send
-            money, buy airtime, request money, check your account balance, and do more with your bank and mobile money
-            accounts. It leverages USSD automation technology to give you more control of your money. No need to
-            remember different USSD codes, switch from bank app to fintech app or biller to biller ever again. Best part
-            is - it works offline.
-          </p>
-          <a href="https://stax.onelink.me/nR0j/ussddirectory" target="_blank" rel="noreferrer">
-            <img className="google-play mr-2 mb-2" src="/uploads/play.png" alt="Google-play" />
-          </a>
-        </div>
-        <img className="gsm" src="/uploads/gsm.png" alt="mobile app view" />
-      </section>
-
-      <section className="wrapper will-grow negate-bottom">
-        <h4 className="co-deepblack mb-5 h2 mt-5h">USSD blog</h4>
-        <div id="recent-blog-posts" className="row overflow-hidden grid mostly-3">
-          <template>
-            <div className="col-lg-3 col-md-5 blog-post-thumb-container">
-              <div className="skewed-bg-bottom-left position-relative mid-grey-bg blog-post-thumb">
-                <a href="" className="stretched-link">
-                  <img src="" className="featured-img" alt="" />
-                  <h3 className="title"></h3>
-                </a>
-              </div>
-            </div>
-          </template>
-        </div>
-        <div className="col-lg-2 col-md-5 home-blog-post-link order-last order-lg-first order-md-6">
-          <a href="https://blog.ussd.directory" className="ff-semibold microtext co-blue mt-5 d-blk tx-r">
-            View all
-          </a>
-        </div>
-      </section>
     </>
   )
 }
