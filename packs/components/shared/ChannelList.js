10000c595e3e05ea03d829be3ba57673c7878e9f
Samuel Ralak <thesamuelralak@gmail.com>
2023-03-27 12:39:18 +0300

    load channelList once

--- packs/components/shared/ChannelList.js
+++ packs/components/shared/ChannelList.js
@@ -1,91 +1,98 @@
-import React, { useEffect, useState } from 'react'
-import { Loader } from './index'
-import PropTypes from 'prop-types'
-import { getChannels } from '../../http'
-import constants from '../../constants'
+import React, { useEffect, useState } from "react";
+import { Loader } from "./index";
+import PropTypes from "prop-types";
+import { getChannels } from "../../http";
+import constants from "../../constants";
 
 const paginationState = {
   page: 1,
   perPage: 100,
-  fetching: false,
-}
+  fetching: false
+};
 
 const ChannelList = ({ channels, country }) => {
-  const currentPath = window.location.pathname
-  const [searchParam, setSearchParam] = useState("")
-  const [displayStyle, setDisplayStyle] = useState('gridstyle')
-  const [pagination, setPagination] = useState(paginationState)
-  const [channelList, setChannelList] = useState({})
-  const [institutionType, setInstitutionType] = useState(constants.INSTITUTION_TYPES.all)
+  const currentPath = window.location.pathname;
+  const [isLoading, setIsLoading] = useState(false)
+  const [searchParam, setSearchParam] = useState("");
+  const [displayStyle, setDisplayStyle] = useState("gridstyle");
+  const [pagination, setPagination] = useState(paginationState);
+  const [channelList, setChannelList] = useState({});
+  const [institutionType, setInstitutionType] = useState(constants.INSTITUTION_TYPES.all);
 
   const onChangeListDisplay = event => {
-    const currentId = event.currentTarget.id
+    const currentId = event.currentTarget.id;
 
-    if (currentId === 'listStyleBtn') {
-      setDisplayStyle('liststyle')
-      return
+    if (currentId === "listStyleBtn") {
+      setDisplayStyle("liststyle");
+      return;
     }
 
-    setDisplayStyle('gridstyle')
-  }
+    setDisplayStyle("gridstyle");
+  };
 
   const onViewMore = async () => {
-    if (currentPath === '/') {
-      window.location.replace('/directory/')
-      return
+    if (currentPath === "/") {
+      window.location.replace("/directory/");
+      return;
     }
 
-    const nextPage = pagination.page + 1
-    let queryParams = `page=${nextPage}&query=${searchParam}`
+    const nextPage = pagination.page + 1;
+    let queryParams = `page=${nextPage}&query=${searchParam}`;
 
     if (country.alpha2) {
-      queryParams = `${queryParams}&country=${country.alpha2}`
+      queryParams = `${queryParams}&country=${country.alpha2}`;
     }
 
-    setPagination({ ...pagination, ...{ fetching: true } })
-    const response = await getChannels(queryParams, pagination.perPage)
+    setPagination({ ...pagination, ...{ fetching: true } });
+    const response = await getChannels(queryParams, pagination.perPage);
 
-    setChannelList({ ...channelList, ...{ data: [...channelList.data, ...response.data.data] } })
-    setPagination({ ...pagination, ...{ page: nextPage, fetching: false } })
-  }
+    setChannelList({ ...channelList, ...{ data: [...channelList.data, ...response.data.data] } });
+    setPagination({ ...pagination, ...{ page: nextPage, fetching: false } });
+  };
 
   const onClickListItem = channel => {
     window.location.replace(
-      `/directory-details/?channelId=${channel.id}&name=${channel.attributes.name.toLowerCase().replace(' ', '-')}`
-    )
-  }
+      `/directory-details/?channelId=${channel.id}&name=${channel.attributes.name.toLowerCase().replace(" ", "-")}`
+    );
+  };
 
   const onInputSearch = async (event) => {
-    const query = event.currentTarget.value.toLowerCase()
-    const queryParams = `query=${query}${country.alpha2 ? '&country=' + country.alpha2 : ''}`
-    setSearchParam(query)
+    setIsLoading(true)
+    const query = event.currentTarget.value.toLowerCase();
+    const queryParams = `query=${query}${country.alpha2 ? "&country=" + country.alpha2 : ""}`;
+    setSearchParam(query);
 
-    const response = await getChannels(queryParams)
-    setChannelList({ ...channelList, data: response.data.data } )
-  }
+    const response = await getChannels(queryParams);
+    setChannelList(response.data);
+    setIsLoading(false)
+  };
 
   const onClickInstitutionType = typeKey => {
     if (constants.INSTITUTION_TYPES[typeKey] === constants.INSTITUTION_TYPES.all) {
-      setChannelList(channels)
+      setChannelList(channels);
     } else {
-      const results = [...channels.data].filter(channel => channel.attributes.institution_type === typeKey)
-      setChannelList({ ...channelList, ...{ data: results } })
+      const results = [...channels.data].filter(channel => channel.attributes.institution_type === typeKey);
+      setChannelList({ ...channelList, ...{ data: results } });
     }
 
-    setInstitutionType(constants.INSTITUTION_TYPES[typeKey])
-  }
+    setInstitutionType(constants.INSTITUTION_TYPES[typeKey]);
+  };
 
-  useEffect(() => setChannelList(channels), [channelList])
+  useEffect(() => {
+    if (Object.keys(channelList).length === 0) {
+      setChannelList(channels);
+    }
+  }, [channels]);
 
   return (
     <>
       <ul className="uk-subnav filter">
         {Object.keys(constants.INSTITUTION_TYPES).map((type, index) => (
           <li
-            className={`sub-filter ${institutionType === constants.INSTITUTION_TYPES[type] ? 'on-active' : ''}`}
+            className={`sub-filter ${institutionType === constants.INSTITUTION_TYPES[type] ? "on-active" : ""}`}
             key={index}
             onClick={() => {
-              onClickInstitutionType(type)
+              onClickInstitutionType(type);
             }}
           >
             <p>{constants.INSTITUTION_TYPES[type]}</p>
@@ -109,11 +116,11 @@ const ChannelList = ({ channels, country }) => {
         </form>
         <section className="code-head">
           <div className="space-between-groups flex-wrap">
-            <p className="ff-bold smalltext no-wrap mb-1 mx-auto ml-0">{country?.name || 'All Countries'}</p>
+            <p className="ff-bold smalltext no-wrap mb-1 mx-auto ml-0">{country?.name || "All Countries"}</p>
             <div className="switcher d-flx flt-r show-mediumup">
               <button
                 id="gridStyleBtn"
-                className={`mr-1 ${displayStyle === 'gridstyle' ? 'active' : ''}`}
+                className={`mr-1 ${displayStyle === "gridstyle" ? "active" : ""}`}
                 onClick={onChangeListDisplay}
               >
                 <p className="mr-1h no-wrap">Grid view</p>
@@ -123,7 +130,7 @@ const ChannelList = ({ channels, country }) => {
               </button>
               <button
                 id="listStyleBtn"
-                className={displayStyle === 'liststyle' ? 'active' : ''}
+                className={displayStyle === "liststyle" ? "active" : ""}
                 onClick={onChangeListDisplay}
               >
                 <p className="mr-1h">List view</p>
@@ -134,7 +141,7 @@ const ChannelList = ({ channels, country }) => {
             </div>
           </div>
           <section>
-            {Object.keys(channelList).length === 0 ? (
+            {(Object.keys(channelList).length === 0) || isLoading ? (
               <Loader />
             ) : (
               <div id="ussdList" className="">
@@ -161,7 +168,7 @@ const ChannelList = ({ channels, country }) => {
               </div>
             )}
           </section>
-          {channelList?.data && channelList.data.length < channelList?.meta?.count && channelList.data.length > 0 && (
+          {channelList?.data && channelList.data.length < channelList?.meta?.count && channelList.data.length > 0 || !isLoading && (
             <a
               onClick={onViewMore}
               className={`ff-medium co-blue mt-3 microtext fit-content mx-auto d-blk`}
@@ -174,16 +181,16 @@ const ChannelList = ({ channels, country }) => {
         </section>
       </div>
     </>
-  )
-}
+  );
+};
 
 ChannelList.propTypes = {
   channels: PropTypes.object.isRequired,
-  country: PropTypes.object,
-}
+  country: PropTypes.object
+};
 
 ChannelList.defaultProps = {
-  country: {},
-}
+  country: {}
+};
 
-export default ChannelList
+export default ChannelList;
